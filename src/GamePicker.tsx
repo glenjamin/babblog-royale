55a961e92f271726d6254e78ff505c86eaa3a5ab
Glen Mailer <glenjamin@gmail.com>
2022-01-11 18:18:43 +0000

    Show MMR delta in game list

--- src/GamePicker.tsx
+++ src/GamePicker.tsx
@@ -15,12 +15,20 @@ function GamePicker({ games, index, onPick }: GamePickerProps): JSX.Element {
       onChange={(e) => onPick(Number(e.target.value))}
     >
       <option value="-1">Select a game</option>
-      {games.map((game, index) => (
-        <option key={index} value={String(index)}>
-          Game #{game.id}: Position {game.you.position} to MRR{" "}
-          {Math.round(game.you.MMR)}
-        </option>
-      ))}
+      {games.map((game, index) => {
+        const {
+          id,
+          you: { position, MMR, oldMMR },
+        } = game;
+        const delta = Math.round(MMR - oldMMR);
+        return (
+          <option key={index} value={String(index)}>
+            Game #{id}: Position {position} to MRR {Math.round(MMR)} (
+            {delta >= 0 && "+"}
+            {delta})
+          </option>
+        );
+      })}
     </Form.Select>
   );
 }
