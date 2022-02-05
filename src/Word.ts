62ee9bc9c6dac4a8c1e0e5fd074c3dcc6171bee7
Endrik Tombak <littleendu@gmail.com>
2022-02-05 12:04:04 +0200

    Function for seeing if word can be expanded

--- src/Word.ts
+++ src/Word.ts
@@ -1,6 +1,11 @@
-import { GameStep } from "./types";
+import { GameStep, Letter, SparseArray } from "./types";
 import words from "./words.json";
-import { horizontalSlice, verticalSlice } from "./utils/gameBoardHelper";
+import {
+  horizontalSlice,
+  isValidMatch,
+  sliceToRegex,
+  verticalSlice,
+} from "./utils/gameBoardHelper";
 
 export type Intention = {
   word: string;
@@ -8,7 +13,7 @@ export type Intention = {
 };
 
 export class Word {
-  private readonly gameStep: GameStep;
+  private readonly letters: SparseArray<Letter>;
   private readonly boardSize: number;
   readonly isHorizontal: boolean;
   /** Index of the axis this word sits in */
@@ -19,66 +24,71 @@ export class Word {
   readonly word: string;
 
   constructor(
-    gameStep: GameStep,
+    letters: SparseArray<Letter>,
     boardSize: number,
     isHorizontal: boolean,
     axisIndex: number,
     middleIndex: number,
     intended: Intention | null
   ) {
-    this.gameStep = gameStep;
+    this.letters = letters;
     this.boardSize = boardSize;
     this.isHorizontal = isHorizontal;
     this.axisIndex = axisIndex;
-    this.startIndex = middleIndex;
-    this.endIndex = middleIndex;
 
-    if (intended === null) {
-      // this is an already placed word
-      // we need to find its start and end index
+    let s = this.mySlice();
 
-      let s = this.mySlice();
-
-      // find start and end index by searching where the word starts and ends
-      for (let i = middleIndex - 1; true; i--) {
-        if (s[i] === undefined) {
-          // the word ended one letter before
-          this.startIndex = i + 1;
-          break;
-        }
+    // find start and end index by searching where the word starts and ends
+    for (
+      let i = intended === null ? middleIndex - 1 : intended.startIndex - 1;
+      true;
+      i--
+    ) {
+      if (s[i] === undefined) {
+        // the word ended one letter before
+        this.startIndex = i + 1;
+        break;
       }
-      for (let i = middleIndex + 1; true; i++) {
-        if (s[i] === undefined) {
-          this.endIndex = i;
-          break;
-        }
+    }
+    for (
+      let i =
+        intended === null
+          ? middleIndex + 1
+          : intended.startIndex + intended.word.length;
+      true;
+      i++
+    ) {
+      if (s[i] === undefined) {
+        this.endIndex = i;
+        break;
       }
-      this.word = s.slice(this.startIndex, this.endIndex).join("");
-    } else {
-      // this is a word that is being placed
-      this.word = intended.word;
-      this.startIndex = intended.startIndex;
-      this.endIndex = intended.startIndex + this.word.length;
+    }
+    let tempSlice = s.slice(this.startIndex, this.endIndex);
+    // replace undefined with placeholder character
+    tempSlice = tempSlice.map((letter) =>
+      letter === undefined ? "a" : letter
+    );
+    this.word = tempSlice.join("");
+
+    if (intended !== null) {
+      let startIndex = this.startIndex - intended.startIndex;
+      this.word =
+        this.word.slice(0, startIndex) +
+        intended.word +
+        this.word.slice(startIndex + intended.word.length);
     }
   }
 
   mySlice() {
     if (this.isHorizontal) {
-      return horizontalSlice(
-        this.gameStep.letters,
-        this.boardSize,
-        this.axisIndex
-      );
+      return horizontalSlice(this.letters, this.boardSize, this.axisIndex);
     } else {
-      return verticalSlice(
-        this.gameStep.letters,
-        this.boardSize,
-        this.axisIndex
-      );
+      return verticalSlice(this.letters, this.boardSize, this.axisIndex);
     }
   }
 
   isInDictionary() {
+    if (this.word.length === 1) return true;
     return words.includes(this.word);
   }
 
@@ -87,17 +97,32 @@ export class Word {
 
     // check if the board is free of other letters
     let s = this.mySlice();
+    let needsPlacementIndices = [];
     for (let i = this.startIndex; i < this.endIndex; i++) {
       // check if the spot is free or if it is the same letter
       if (s[i] !== undefined && s[i] !== this.word[i - this.startIndex]) {
         return false;
+      } else if (s[i] === undefined) {
+        needsPlacementIndices.push(i);
       }
     }
 
-    // check surrounding letters and the words they make up
-    for (let i = this.startIndex; i < this.endIndex; i++) {
+    // if the word can be placed, simulate placement
+    let simulated = [...this.letters];
+    for (let i = 0; i < needsPlacementIndices.length; i++) {
+      if (this.isHorizontal) {
+        simulated[this.boardSize * this.axisIndex + needsPlacementIndices[i]] =
+          this.word[needsPlacementIndices[i] - this.startIndex] as Letter;
+      } else {
+        simulated[this.boardSize * needsPlacementIndices[i] + this.axisIndex] =
+          this.word[needsPlacementIndices[i] - this.startIndex] as Letter;
+      }
+    }
+
+    // check surrounding letters and the words they make up in the simulated board
+    for (let i of needsPlacementIndices) {
       let testWord = new Word(
-        this.gameStep,
+        simulated,
         this.boardSize,
         !this.isHorizontal, // intentionally reversed
         i, // intentionally switched from axisIndex
@@ -115,9 +140,42 @@ export class Word {
     let rv = [];
     let sliceFunction = this.isHorizontal ? verticalSlice : horizontalSlice;
     for (let i = this.startIndex; i < this.endIndex; i++) {
-      rv.push(sliceFunction(this.gameStep.letters, this.boardSize, i));
+      rv.push(sliceFunction(this.letters, this.boardSize, i));
     }
     return rv;
+  }
+
+  /**
+   * Returns all the words this word could be expanded into
+   */
+  async *getExpansionOptions(playerRack: Array<Letter>) {
+    // no expansion is also a valid option
+    yield this;
+
+    let re = sliceToRegex(this.mySlice(), this.startIndex, playerRack);
+    for (const word of words) {
+      if (isValidMatch(word.match(re), playerRack)) {
+        const wordOnlyRe = new RegExp(this.word, "g");
+        for (const match of Array.from(word.matchAll(wordOnlyRe))) {
+          // find start index of the word
+          let startIndex = this.startIndex - match.index!;
+          let wordObj = new Word(
+            this.letters,
+            this.boardSize,
+            this.isHorizontal,
+            this.axisIndex,
+            this.startIndex,
+            {
+              word,
+              startIndex: startIndex,
+            }
+          );
+          if (wordObj.isValidForPlacement()) {
+            yield wordObj;
+          }
+        }
+      }
+    }
   }
 }
 
@@ -132,7 +190,7 @@ export function findPlayerWord(gameStep: GameStep, boardSize: number) {
     [axisIndex, middleIndex] = [middleIndex, axisIndex];
   }
   return new Word(
-    gameStep,
+    gameStep.letters,
     boardSize,
     isHorizontal,
     axisIndex,
