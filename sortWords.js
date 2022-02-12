15df77cdb009bd8a33302842849c6df1d743ea21
Endrik Tombak <littleendu@gmail.com>
2022-02-12 12:34:51 +0200

    Sort words based on their initial value

---
+++ sortWords.js
@@ -0,0 +1,28 @@
+// @ts-nocheck
+const fs = require("fs");
+
+process.env.BABEL_ENV ??= "test";
+require("@babel/register")({
+  presets: ["react-app"],
+  extensions: [".ts"]
+});
+
+const { tileValues } = require("./src/constants");
+
+const wordsFile = "./words.json";
+
+const words = JSON.parse(fs.readFileSync(wordsFile, "utf8"));
+
+const sorted = words.sort((a, b) => {
+  let aValue = 0;
+  let bValue = 0;
+  for (let i = 0; i < a.length; i++) {
+    aValue += tileValues(a[i]);
+  }
+  for (let i = 0; i < b.length; i++) {
+    bValue += tileValues(b[i]);
+  }
+  return aValue - bValue;
+}).reverse();
+
+fs.writeFileSync(wordsFile, JSON.stringify(sorted));
